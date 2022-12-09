579f85ca5e31bb5ea75e9017088b22ac86c40a56
vishalbiswakarma <118849239+vishalbiswakarma@users.noreply.github.com>
2022-12-09 11:42:22 +0530

    Add files via upload

---
+++ python project.py
@@ -0,0 +1,23 @@
+print("\t\t\t  ---FIBONACCI SERIES---")
+k=(input("Enter the number's that you want to check whether it is a vaild number in a Fibonacci Series or not: ",))
+li1=k.split(",")
+li2=[]
+for i in li1:
+    li2.append(int(i))
+m=max(li2)
+n=2*m
+sum1=0
+a=0
+b=1
+li3=[]
+while(sum1<n):
+    li3.append(sum1)
+    a=b
+    b=sum1
+    sum1=a+b
+for i in li2:
+    if i in li3:
+        print(i,"is valid")
+    else:
+        print(i,"is not valid")
+print("\n\t\t\t     ---Thankyou---")
